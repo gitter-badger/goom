71db16c21aa9361da9c465687aa490b753df8cb4
Benoit Benedetti <humboldtux@gmail.com>
2015-01-02 11:11:10 +0100

    Add Viper conf tests

---
+++ conf_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/spf13/viper"
+)
+
+func TestDefaultEnvConf(t *testing.T) {
+	goomPath := viper.Get("goomPath")
+	if goomPath == nil {
+		t.Fatalf(`goomPath should be defined by default`)
+	}
+
+	configPath := viper.Get("configPath")
+	if configPath == nil {
+		t.Fatalf(`configPath should be defined by default`)
+	}
+
+	dataPath := viper.Get("dataPath")
+	if dataPath == nil {
+		t.Fatalf(`dataPath should be defined by default`)
+	}
+
+	boltdbPath := viper.Get("boltdbPath")
+	if boltdbPath == nil {
+		t.Fatalf(`boltdbPath should be defined by default`)
+	}
+
+}
